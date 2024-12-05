e065569c50818a77c763ee2de85d3cd3abb6697f
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-05 13:19:49 +0100

    add ResourcesPolicy model with schema definition and interface

---
+++ src/models/ResourcesPolicy.ts
@@ -0,0 +1,18 @@
+import mongoose, { Document } from 'mongoose';
+
+export interface IResourcesPolicy extends Document {
+	_id: string;
+	name: string;
+	description: string;
+	accessLevel: number;
+	authorizations: object;
+}
+
+const resourcesPolicySchema = new mongoose.Schema<IResourcesPolicy>({
+	name: { type: String, required: true },
+	description: { type: String, required: true },
+	accessLevel: { type: Number, required: true },
+	authorizations: { type: Object, required: true },
+});
+
+export const ResourcesPolicy = mongoose.models.ResourcesPolicy || mongoose.model<IResourcesPolicy>('ResourcesPolicy', resourcesPolicySchema);
