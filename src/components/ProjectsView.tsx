e2eb361ce1148e056bbfdb1f01b94fce1fc5807d
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-08 00:19:49 +0100

    feat: enhance ProjectsView to display project members with avatars and fallback

--- src/components/ProjectsView.tsx
+++ src/components/ProjectsView.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { LayoutGrid, List } from 'lucide-react';
+import { Ellipsis, LayoutGrid, List, UserRound } from 'lucide-react';
 import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { Card, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
@@ -9,9 +9,9 @@ import { ProjectType } from '@/types/project';
 import CreateProjectForm from './forms/CreateProjectForm';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 export default function ProjectsView({ projects }: { projects: ProjectType[] }) {
-	const [viewMode, setViewMode] = useState<'grid' | 'list'>('grid');
 	const router = useRouter();
 
 	const handleProjectCreated = () => {
@@ -22,55 +22,52 @@ export default function ProjectsView({ projects }: { projects: ProjectType[] }) 
 		<div>
 			<div className="flex justify-between items-center mb-8">
 				<h1 className="text-3xl font-bold">Projects</h1>
-				<div className="flex items-center gap-4">
-					<ToggleGroup type="single" value={viewMode} onValueChange={(value: any) => setViewMode(value)}>
-						<ToggleGroupItem value="grid" aria-label="Grid view">
-							<LayoutGrid className="h-4 w-4" />
-						</ToggleGroupItem>
-						<ToggleGroupItem value="list" aria-label="List view">
-							<List className="h-4 w-4" />
-						</ToggleGroupItem>
-					</ToggleGroup>
-					<CreateProjectForm onProjectCreated={handleProjectCreated} />
-				</div>
+				<CreateProjectForm onProjectCreated={handleProjectCreated} />
 			</div>
-			{JSON.stringify(projects)}
-
-			{viewMode === 'list' ? (
-				<Table>
-					<TableHeader>
-						<TableRow>
-							<TableCell>Name</TableCell>
-							<TableCell>Description</TableCell>
-							<TableCell>Created</TableCell>
-						</TableRow>
-					</TableHeader>
-					<TableBody>
-						{projects.map((project, index) => (
-							<TableRow key={index}>
-								<TableCell className="font-medium">
-									<Link href={project.slug}>{project.name}</Link>
-								</TableCell>
-								<TableCell>{project.description}</TableCell>
-								<TableCell>{new Date(project.createdAt).toLocaleDateString()}</TableCell>
-							</TableRow>
-						))}
-					</TableBody>
-				</Table>
-			) : (
-				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-					{projects.map((project, index) => (
-						<Link href={project.slug} key={index}>
-							<Card className="hover:shadow-lg transition-shadow">
-								<CardHeader>
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+				{projects.map((project, index) => (
+					<Link href={project.slug} key={index}>
+						<Card className="hover:shadow-lg transition-shadow">
+							<CardHeader className="space-y-0 flex flex-row justify-between">
+								<div>
 									<CardTitle className="text-xl">{project.name}</CardTitle>
-									<CardDescription className="mt-2">{project.description}</CardDescription>
-								</CardHeader>
-							</Card>
-						</Link>
-					))}
-				</div>
-			)}
+									<CardDescription>{project.description}</CardDescription>
+								</div>
+								<div className="-mt-1">
+									{project.members.length > 0 ? (
+										<div className="flex items-center">
+											{project.members.slice(0, 4).map((user, userIndex) => (
+												<div key={userIndex} className="flex items-center justify-center bg-background rounded-full w-12 h-12 -ml-6 z-40">
+													<Avatar className="border shadow-sm w-10 h-10">
+														<AvatarImage src={user.image} alt={user.username} />
+														<AvatarFallback>
+															{user.username
+																?.split(' ')
+																.map((word) => word.charAt(0).toUpperCase())
+																.join('')}
+														</AvatarFallback>
+													</Avatar>
+												</div>
+											))}
+											{project.members.length > 4 && (
+												<div className="flex items-center justify-center bg-background rounded-full w-12 h-12 -ml-6 z-40">
+													<span className="rounded-full border shadow-sm flex items-center justify-center w-10 h-10 bg-muted">
+														<Ellipsis className="w-5 h-5" />
+													</span>
+												</div>
+											)}
+										</div>
+									) : (
+										<span className="rounded-full border shadow-sm w-10 h-10 flex items-center justify-center -ml-1">
+											<UserRound className="w-5 h-5" />
+										</span>
+									)}
+								</div>
+							</CardHeader>
+						</Card>
+					</Link>
+				))}
+			</div>
 		</div>
 	);
 }
