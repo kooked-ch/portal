3c898f4ca52a1e3b13aaf989650f4edc6c94d518
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-07 18:51:57 +0100

    feat: rename createDate field to createdAt in Project model and schema

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -5,7 +5,7 @@ export interface IProject extends Document {
 	name: string;
 	description: string;
 	slug: string;
-	createDate: Date;
+	createdAt: Date;
 	members: {
 		userId: string;
 		accreditation: string;
@@ -16,7 +16,7 @@ const projectSchema = new mongoose.Schema<IProject>({
 	name: { type: String, required: true },
 	description: { type: String, required: true },
 	slug: { type: String, required: true },
-	createDate: { type: Date, required: true, default: Date.now },
+	createdAt: { type: Date, required: true, default: Date.now },
 	members: [{ userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, accreditation: { type: mongoose.Schema.Types.ObjectId, ref: 'Accreditation' } }],
 });
 
