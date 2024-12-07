8be0b18e18bf5ff99e730072f0f59822154ac2c2
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-07 18:51:52 +0100

    feat: add createdAt field to ProjectType and enhance validation messages in projectSchema

--- src/types/project.ts
+++ src/types/project.ts
@@ -1,13 +1,13 @@
 import { z } from 'zod';
 
 export interface ProjectType {
-	id: string;
 	name: string;
 	description: string;
 	slug: string;
+	createdAt: Date;
 }
 
 export const projectSchema = z.object({
-	name: z.string().min(3).max(255),
-	description: z.string().min(3).max(255),
+	name: z.string().min(3, 'Name must contain at least 3 characters').max(255, 'Name must contain at most 255 characters'),
+	description: z.string().min(3, 'Description must contain at least 3 characters').max(255, 'Description must contain at most 255 characters'),
 });
