9c8f40d1131b587206233b8ab2f85d21e577cb94
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-07 13:52:55 +0100

    update accreditation type to object in next-auth types definition

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -7,7 +7,7 @@ declare module 'next-auth' {
 			name?: string | null;
 			email?: string | null;
 			image?: string | null;
-			accreditation?: string | null;
+			accreditation: object;
 			isTwoFactorComplete: boolean;
 		};
 	}
