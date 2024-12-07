3bdb51545ca5b6dd7f3b0f1340558ba15b5a2f3f
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-07 18:50:47 +0100

    feat: add slug and createDate fields to project model

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -4,6 +4,8 @@ export interface IProject extends Document {
 	_id: string;
 	name: string;
 	description: string;
+	slug: string;
+	createDate: Date;
 	members: {
 		userId: string;
 		accreditation: string;
@@ -13,6 +15,8 @@ export interface IProject extends Document {
 const projectSchema = new mongoose.Schema<IProject>({
 	name: { type: String, required: true },
 	description: { type: String, required: true },
+	slug: { type: String, required: true },
+	createDate: { type: Date, required: true, default: Date.now },
 	members: [{ userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, accreditation: { type: mongoose.Schema.Types.ObjectId, ref: 'Accreditation' } }],
 });
 
