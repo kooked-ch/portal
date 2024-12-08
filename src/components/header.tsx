f045a2c3b8c33b99c21ac158320807ebc06d2b14
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-08 20:25:50 +0100

    feat: update Header component to include project selection dropdown and navigate on project change

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,20 @@
+'use client';
 import { User } from 'next-auth';
 import * as React from 'react';
+import { usePathname, useRouter } from 'next/navigation';
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from '@/components/ui/select';
-import { Container, Settings } from 'lucide-react';
+import { Container } from 'lucide-react';
 import { UserDropdown } from './userDropdown';
+import { ProjectsType } from '@/types/project';
 
-export async function Header({ user }: { user: User }) {
+export function Header({ user, projects }: { user: User; projects: ProjectsType[] | null }) {
+	const router = useRouter();
+	const pathname = usePathname();
+
+	const onProjectChange = (project: string) => {
+		router.push(`/${project}`);
+	};
+
 	return (
 		<header className="border-b p-4">
 			<nav className="max-w-[1400px] mx-auto flex items-center justify-between">
@@ -12,24 +22,25 @@ export async function Header({ user }: { user: User }) {
 					<Container className="w-8 h-8 text-primary" />
 					<h1 className="text-2xl font-bold uppercase">Portal</h1>
 				</div>
-				<div className="flex items-center gap-3">
-					<Select>
-						<SelectTrigger className="w-[180px]">
-							<SelectValue placeholder="Select a fruit" />
-						</SelectTrigger>
-						<SelectContent>
-							<SelectGroup>
-								<SelectLabel>Fruits</SelectLabel>
-								<SelectItem value="apple">Apple</SelectItem>
-								<SelectItem value="banana">Banana</SelectItem>
-								<SelectItem value="blueberry">Blueberry</SelectItem>
-								<SelectItem value="grapes">Grapes</SelectItem>
-								<SelectItem value="pineapple">Pineapple</SelectItem>
-							</SelectGroup>
-						</SelectContent>
-					</Select>
-					<UserDropdown user={user} />
-				</div>
+				{projects && (
+					<div className="flex items-center gap-3">
+						<Select onValueChange={onProjectChange} defaultValue={projects.find((project) => pathname.includes(`/${project.name}`))?.name}>
+							<SelectTrigger className="w-[180px]">
+								<SelectValue placeholder="Select a project" />
+							</SelectTrigger>
+							<SelectContent>
+								<SelectGroup>
+									{projects.map((project, index) => (
+										<SelectItem key={index} value={project.name}>
+											{project.name}
+										</SelectItem>
+									))}
+								</SelectGroup>
+							</SelectContent>
+						</Select>
+						<UserDropdown user={user} />
+					</div>
+				)}
 			</nav>
 		</header>
 	);
