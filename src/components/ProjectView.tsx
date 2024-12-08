6e487294554ef018ec054a7cbd1c07de53bd5f77
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-08 20:54:10 +0100

    feat: add ProjectView component to display project details and associated applications

---
+++ src/components/ProjectView.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { Card, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
+import { ProjectType } from '@/types/project';
+import Link from 'next/link';
+
+export default function ProjectView({ project }: { project: ProjectType }) {
+	return (
+		<div>
+			<div className="flex justify-between items-center mb-8">
+				<h1 className="text-3xl font-bold">{project.name}</h1>
+			</div>
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+				{project.apps.map((app, index) => (
+					<Link href={project.slug + '/' + app.name} key={index}>
+						<Card className="hover:shadow-lg transition-shadow">
+							<CardHeader className="space-y-0 flex flex-row justify-between">
+								<div>
+									<CardTitle className="text-xl">{app.name}</CardTitle>
+									<CardDescription>{app.description}</CardDescription>
+								</div>
+							</CardHeader>
+						</Card>
+					</Link>
+				))}
+			</div>
+		</div>
+	);
+}
