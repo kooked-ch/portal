b84f6a950f7ee1e28f1db19e36054aa56481bb05
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-08 20:26:04 +0100

    feat: add AppsType and AppType interfaces to define application structure

---
+++ src/types/app.ts
@@ -0,0 +1,45 @@
+export interface AppsType {
+	name: string;
+	description: string;
+	createdAt: Date;
+	repository: {
+		url: string;
+		image: string;
+		version: string;
+	};
+}
+
+export interface AppType {
+	name: string;
+	description: string;
+	createdAt: Date;
+	repository: {
+		url: string;
+		image: string;
+		version: string;
+	};
+	replicas: number;
+	domains: {
+		url: string;
+		port: number;
+		container: string;
+	}[];
+	databases: {
+		name: string;
+		password: string;
+		provider: string;
+		user: string;
+	}[];
+	containers: {
+		name: string;
+		image: string;
+		env: {
+			key: string;
+			value: string;
+		}[];
+	}[];
+	members: {
+		username: string;
+		image: string;
+	}[];
+}
