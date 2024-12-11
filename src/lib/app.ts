6dd24670e405c5358f1b27340ffb79f9acb09830
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-11 20:35:22 +0100

    feat: implement createApp function for application creation with validation and error handling

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -2,6 +2,10 @@ import { AppType } from '@/types/app';
 import { customObjectsApi } from './api';
 import { checkAccreditation } from './auth';
 import { getRepository } from './utils';
+import { ErrorType } from '@/types/error';
+import { AppModel } from '@/models/App';
+import { ProjectModel } from '@/models/Project';
+import { AccreditationModel } from '@/models/Accreditation';
 
 export async function getApp(projectName: string, appname: string): Promise<AppType | null> {
 	const hasAccess = await checkAccreditation('apps:2:read', `${projectName}/${appname}`);
@@ -26,4 +30,43 @@ export async function getApp(projectName: string, appname: string): Promise<AppT
 		console.error('Error fetching app:', error);
 		return null;
 	}
+}
+
+export async function createApp(userId: string, { name, description, projectName }: { name: string; description: string; projectName: string }): Promise<ErrorType> {
+	const existingApp = await AppModel.findOne({ name });
+	if (existingApp) {
+		return {
+			message: 'App already exists',
+			status: 400,
+		};
+	}
+
+	await customObjectsApi.createNamespacedCustomObject('kooked.ch', 'v1', projectName, 'kookedapps', {
+		apiVersion: 'kooked.ch/v1',
+		kind: 'KookedApp',
+		metadata: {
+			name,
+			annotations: {
+				description,
+			},
+		},
+	});
+
+	const project = await ProjectModel.findOne({ slug: projectName }).exec();
+	const defaultAccreditation = await AccreditationModel.findOne({ slug: 'own', accessLevel: 2 }).exec();
+	if (!defaultAccreditation) {
+		throw new Error('Default accreditation not found');
+	}
+
+	await AppModel.create({
+		name,
+		projectId: project?._id,
+		image: '',
+		collaborators: [{ userId, accreditation: defaultAccreditation._id }],
+	});
+
+	return {
+		message: 'App created',
+		status: 200,
+	};
 }
