44cdf0c611e72f1b9d1dc77cb6411eb05dcef076
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-11 20:36:13 +0100

    feat: add validation schema for AppsType using zod

--- src/types/app.ts
+++ src/types/app.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod';
+
 export interface AppsType {
 	name: string;
 	description: string;
@@ -8,6 +10,11 @@ export interface AppsType {
 		version: string;
 	};
 }
+
+export const appSchema = z.object({
+	name: z.string().min(3, 'Name must contain at least 3 characters').max(255, 'Name must contain at most 255 characters'),
+	description: z.string().min(3, 'Description must contain at least 3 characters').max(255, 'Description must contain at most 255 characters'),
+});
 
 export interface AppType {
 	name: string;
