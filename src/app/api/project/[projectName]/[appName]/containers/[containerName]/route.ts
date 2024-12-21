acd158a7d7ae4990dab2b1f1cea6ce573b58f087
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-21 17:48:33 +0100

    feat: implement updateContainer API endpoint with validation and accreditation checks

--- src/app/api/project/[projectName]/[appName]/containers/[containerName]/route.ts
+++ src/app/api/project/[projectName]/[appName]/containers/[containerName]/route.ts
@@ -1,6 +1,35 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { checkAccreditation, getUser } from '@/lib/auth';
-import { deleteContainer } from '@/lib/container';
+import { deleteContainer, updateContainer } from '@/lib/container';
+import { containerSchema } from '@/types/container';
+
+export async function PUT(req: NextRequest, { params }: { params: { projectName: string; appName: string; containerName: string } }) {
+	try {
+		const user = await getUser();
+
+		if (!user || !(await checkAccreditation('containers:2:update', `${params.projectName}/${params.appName}`))) {
+			return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+		}
+
+		const { name, image, version, env } = await req.json();
+
+		if (env && Array.isArray(env) && (env.filter((e) => e.name.trim() === '' && e.value.trim() !== '').length > 0 || env.filter((e) => e.name.trim() !== '' && e.value.trim() === '').length > 0)) {
+			return NextResponse.json({ message: 'Environment variables must have both a name and a value' }, { status: 400 });
+		}
+
+		const validationResult = containerSchema.safeParse({ name, image, version });
+		if (!validationResult.success) {
+			return NextResponse.json({ message: 'Invalid request', details: validationResult.error.errors }, { status: 400 });
+		}
+
+		const container = await updateContainer({ projectName: params.projectName, appName: params.appName, containerName: params.containerName, data: { name, image, version, env } });
+
+		return NextResponse.json({ message: container.message }, { status: container.status });
+	} catch (error) {
+		console.error('Error creating project:', error);
+		return NextResponse.json({ message: 'An unexpected error occurred' }, { status: 500 });
+	}
+}
 
 export async function DELETE(req: NextRequest, { params }: { params: { projectName: string; appName: string; containerName: string } }) {
 	try {
