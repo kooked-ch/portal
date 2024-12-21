6098be37044309a6d473cdf53a89e5b0792173ab
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-21 17:46:24 +0100

    feat: add updateContainer function to modify existing containers with accreditation checks

--- src/lib/container.ts
+++ src/lib/container.ts
@@ -1,5 +1,6 @@
 import { customObjectsApi } from './api';
 import { ErrorType } from '@/types/error';
+import { checkAccreditation } from './auth';
 
 export async function createContainer({ projectName, appName, name, image, version, env }: { projectName: string; appName: string; name: string; image: string; version: string; env: { name: string; value: string }[] }): Promise<ErrorType> {
 	try {
@@ -43,6 +44,52 @@ export async function createContainer({ projectName, appName, name, image, versi
 		};
 	} catch (error: unknown) {
 		console.error('Error creating container:', error);
+		return { message: 'An unexpected error occurred', status: 500 };
+	}
+}
+
+export async function updateContainer({ projectName, appName, containerName, data }: { projectName: string; appName: string; containerName: string; data: { name: string; image: string; version: string; env: { name: string; value: string }[] } }): Promise<ErrorType> {
+	try {
+		const app: any = await customObjectsApi.getNamespacedCustomObject('kooked.ch', 'v1', projectName, 'kookedapps', appName);
+
+		if (!app || !app.body || !app.body.spec) {
+			return {
+				message: 'App not found',
+				status: 404,
+			};
+		}
+
+		const containerIndex = app.body.spec.containers.findIndex((container: any) => container.name === containerName);
+		const container = app.body.spec.containers[containerIndex];
+
+		if (containerIndex === -1) {
+			return { message: 'Container not found', status: 404 };
+		}
+
+		const canUpdateEnv = await checkAccreditation('env:2:update', `${projectName}/${appName}`);
+
+		const patch = [
+			{
+				op: 'replace',
+				path: `/spec/containers/${containerIndex}`,
+				value: {
+					name: data.name,
+					image: `${data.image}:${data.version}`,
+					env: canUpdateEnv ? (data.env.some((envVar) => envVar.name.trim() === '') ? undefined : data.env) : container.env,
+				},
+			},
+		];
+
+		const options = { headers: { 'Content-type': 'application/json-patch+json' } };
+
+		await customObjectsApi.patchNamespacedCustomObject('kooked.ch', 'v1', projectName, 'kookedapps', appName, patch, undefined, undefined, undefined, options);
+
+		return {
+			message: 'Container updated',
+			status: 200,
+		};
+	} catch (error: unknown) {
+		console.error('Error updating container:', error);
 		return { message: 'An unexpected error occurred', status: 500 };
 	}
 }
