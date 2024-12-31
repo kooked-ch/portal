b0e67df72451538e21c84070c85885df0fba6b97
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-31 16:28:27 +0100

    feat: add updateDatabase and deleteDatabase functions for managing database operations

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -64,3 +64,84 @@ export async function createDatabase(projectName: string, appName: string, data:
 		return { message: 'An unexpected error occurred', status: 500 };
 	}
 }
+
+export async function updateDatabase(projectName: string, appName: string, data: DatabaseType): Promise<ErrorType> {
+	try {
+		const app: any = await customObjectsApi.getNamespacedCustomObject({ group: 'kooked.ch', version: 'v1', namespace: projectName, plural: 'kookedapps', name: appName });
+
+		if (!app) {
+			return {
+				message: 'App not found',
+				status: 404,
+			};
+		}
+
+		const databaseIndex = app.spec.databases.findIndex((database: any) => database.name === data.name);
+		if (databaseIndex === -1) {
+			return { message: 'Database not found', status: 404 };
+		}
+
+		const database = app.spec.databases[databaseIndex];
+
+		const patch = [
+			{
+				op: 'replace',
+				path: `/spec/databases/${databaseIndex}`,
+				value: {
+					name: data.name,
+					type: data.provider,
+					user: data.username,
+					password: data.password,
+				},
+			},
+		];
+
+		await customObjectsApi.patchNamespacedCustomObject({ group: 'kooked.ch', version: 'v1', namespace: projectName, plural: 'kookedapps', name: appName, body: patch });
+		await log(`Updated  ${data.provider} database: ${data.name}`, 'info', projectName, appName);
+
+		return {
+			message: 'Database updated',
+			status: 200,
+		};
+	} catch (error: unknown) {
+		console.error('Error updating database:', error);
+		return { message: 'An unexpected error occurred', status: 500 };
+	}
+}
+
+export async function deleteDatabase(projectName: string, appName: string, databaseName: string): Promise<ErrorType> {
+	try {
+		const app: any = await customObjectsApi.getNamespacedCustomObject({ group: 'kooked.ch', version: 'v1', namespace: projectName, plural: 'kookedapps', name: appName });
+
+		if (!app) {
+			return {
+				message: 'App not found',
+				status: 404,
+			};
+		}
+
+		const databaseIndex = app.spec.databases.findIndex((database: any) => database.name === databaseName);
+
+		if (databaseIndex === -1) {
+			return { message: 'Database not found', status: 404 };
+		}
+
+		const patch = [
+			{
+				op: 'remove',
+				path: `/spec/databases/${databaseIndex}`,
+			},
+		];
+
+		await customObjectsApi.patchNamespacedCustomObject({ group: 'kooked.ch', version: 'v1', namespace: projectName, plural: 'kookedapps', name: appName, body: patch });
+		await log(`Deleted  ${databaseName} database`, 'info', projectName, appName);
+
+		return {
+			message: 'Database deleted',
+			status: 200,
+		};
+	} catch (error: unknown) {
+		console.error('Error deleting database:', error);
+		return { message: 'An unexpected error occurred', status: 500 };
+	}
+}
