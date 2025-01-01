d1b81adaf51b581e60c21ca119839610c01a1fcc
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-01 21:05:02 +0100

    feat: add verification page for two-factor authentication with redirect logic

---
+++ src/app/(auth)/factor/page.tsx
@@ -0,0 +1,19 @@
+import TwoFactorVerify from '@/components/TwoFactorVerify';
+import { getTwoFactor } from '@/lib/factor';
+import { redirect } from 'next/navigation';
+
+export default async function VerifyPage({ params, searchParams }: { params: { slug: string }; searchParams?: { [key: string]: string | string[] | undefined } }) {
+	const { enabled, disabled } = await getTwoFactor();
+
+	const redirectPath = Array.isArray(searchParams?.callbackUrl) ? searchParams.callbackUrl[0] : searchParams?.callbackUrl || '/';
+
+	if (disabled) {
+		redirect('/');
+	}
+
+	if (!disabled && !enabled) {
+		redirect(`/enable?callbackUrl=${encodeURIComponent(redirectPath)}`);
+	}
+
+	return <TwoFactorVerify />;
+}
