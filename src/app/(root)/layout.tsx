9fc16f8cb1480dd34e276fd2229a0629f2800428
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-01 21:04:44 +0100

    feat: add redirect to login for unauthenticated users in RootLayout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,13 +1,14 @@
 import { ReactNode } from 'react';
 import React from 'react';
-import { User } from 'next-auth';
 import { Header } from '@/components/header';
 import { getUser } from '@/lib/auth';
 import { ProjectsType } from '@/types/project';
 import { getProjects } from '@/lib/project';
+import { redirect } from 'next/navigation';
 
 export default async function RootLayout({ children }: { children: ReactNode }) {
-	const user: User = await getUser();
+	const user = await getUser();
+	if (!user) redirect('/login');
 	const projects: ProjectsType[] | null = await getProjects(user.id);
 
 	return (
