53f292437abf9a5faf820a194459e6ebee1a0263
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-02 14:47:52 +0100

    fix: enhance layout in LogViewer component for improved responsiveness and organization

--- src/components/log.tsx
+++ src/components/log.tsx
@@ -33,48 +33,56 @@ export default function LogViewer({ logs, collaborators }: { logs: logType[]; co
 
 	return (
 		<div className="space-y-4">
-			<div className="flex gap-2">
-				<div className="max-w-[22rem] w-full">
-					<div className="relative h-10">
-						<Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 z-10 -mt-0.5 w-4 h-4" />
-						<Input placeholder="Search logs..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} className="flex-1 pl-8 py-2" />
+			<div className="flex gap-2 md:flex-row flex-col">
+				<div className="flex flex-row md:flex-col gap-2 md:max-w-[22rem] w-full">
+					<div className="w-full">
+						<div className="relative h-10">
+							<Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 z-10 -mt-0.5 w-4 h-4" />
+							<Input placeholder="Search logs..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} className="flex-1 pl-8 py-2" />
+						</div>
 					</div>
+					<Button variant="outline" size="sm" className="flex md:hidden items-center justify-start h-9 w-32" onClick={() => setDateRange((prev) => (prev === 7 ? 30 : 7))}>
+						<Calendar className="w-4 h-4" />
+						<span>{dateRange === 7 ? 'Last 30 Days' : 'Last 7 Days'}</span>
+					</Button>
 				</div>
-				<Select onValueChange={(value) => setSelectedCollaborator(value)} defaultValue="">
-					<SelectTrigger className="w-full sm:w-52">
-						<SelectValue placeholder="Filter by collaborator" />
-					</SelectTrigger>
-					<SelectContent>
-						<SelectItem value="all">All Collaborators</SelectItem>
-						{collaborators.map((collaborator, index) => (
-							<SelectItem key={index} value={collaborator.username}>
-								{collaborator.username}
-							</SelectItem>
-						))}
-					</SelectContent>
-				</Select>
+				<div className="flex gap-2">
+					<Select onValueChange={(value) => setSelectedCollaborator(value)} defaultValue="">
+						<SelectTrigger className="w-full">
+							<SelectValue placeholder="Filter by collaborator" />
+						</SelectTrigger>
+						<SelectContent>
+							<SelectItem value="all">All Collaborators</SelectItem>
+							{collaborators.map((collaborator, index) => (
+								<SelectItem key={index} value={collaborator.username}>
+									{collaborator.username}
+								</SelectItem>
+							))}
+						</SelectContent>
+					</Select>
 
-				<Select onValueChange={(value) => setSortOrder(value as 'asc' | 'desc')} defaultValue="desc">
-					<SelectTrigger className="w-full sm:w-32">
-						<SelectValue placeholder="Sort by date" />
-					</SelectTrigger>
-					<SelectContent>
-						<SelectItem value="asc">Ascending</SelectItem>
-						<SelectItem value="desc">Descending</SelectItem>
-					</SelectContent>
-				</Select>
+					<Select onValueChange={(value) => setSortOrder(value as 'asc' | 'desc')} defaultValue="desc">
+						<SelectTrigger className="w-full">
+							<SelectValue placeholder="Sort by date" />
+						</SelectTrigger>
+						<SelectContent>
+							<SelectItem value="asc">Ascending</SelectItem>
+							<SelectItem value="desc">Descending</SelectItem>
+						</SelectContent>
+					</Select>
 
-				<Button variant="outline" size="sm" className="flex items-center justify-start h-9 w-32" onClick={() => setDateRange((prev) => (prev === 7 ? 30 : 7))}>
-					<Calendar className="w-4 h-4" />
-					<span>{dateRange === 7 ? 'Last 30 Days' : 'Last 7 Days'}</span>
-				</Button>
+					<Button variant="outline" size="sm" className="hidden md:flex items-center justify-start h-9 w-32" onClick={() => setDateRange((prev) => (prev === 7 ? 30 : 7))}>
+						<Calendar className="w-4 h-4" />
+						<span>{dateRange === 7 ? 'Last 30 Days' : 'Last 7 Days'}</span>
+					</Button>
+				</div>
 			</div>
 
 			<div className="bg-[#1E1E20] p-4 rounded-lg overflow-y-auto max-h-96">
 				{filteredLogs.length > 0 ? (
 					<ul className="space-y-2">
 						{filteredLogs.map((log, index) => (
-							<li key={index} className="flex items-center justify-between border-b border-[#333] pb-2">
+							<li key={index} className="flex md:items-center items-start md:justify-between justify-start flex-col md:flex-row border-b border-[#333] pb-2">
 								<span className="text-sm text-white">{log.message}</span>
 								<span className="text-xs text-[#666]">
 									{log.user} - {new Date(log.date).toLocaleString()}
