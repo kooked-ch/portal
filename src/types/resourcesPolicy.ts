79f4d47db0c50e4ebe737b48f24aadb030d04afe
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-02 23:21:48 +0100

    feat: add TypeScript interfaces for resource policies of projects and apps

---
+++ src/types/resourcesPolicy.ts
@@ -0,0 +1,34 @@
+export interface ProjectsResourcesPolicy {
+	name: string;
+	description: string;
+	totalLimit: number;
+	remainingLimit: number;
+}
+
+export interface ProjectResourcesPolicy {
+	name: string;
+	description: string;
+	totalLimit: number;
+	remainingLimit: number;
+}
+
+export interface AppResourcesPolicy {
+	containers: {
+		name: string;
+		description: string;
+		totalLimit: number;
+		remainingLimit: number;
+	};
+	databases: {
+		name: string;
+		description: string;
+		totalLimit: number;
+		remainingLimit: number;
+	};
+	domains: {
+		name: string;
+		description: string;
+		totalLimit: number;
+		remainingLimit: number;
+	};
+}
