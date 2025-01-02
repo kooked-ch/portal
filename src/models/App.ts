7f18b269a2f1d92fa3a7a5a543fc3420f3542667
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-02 23:22:18 +0100

    feat: add resourcesPolicy field to IApp interface and schema

--- src/models/App.ts
+++ src/models/App.ts
@@ -5,6 +5,11 @@ export interface IApp extends Document {
 	name: string;
 	projectId: String;
 	image: string;
+	resourcesPolicy: {
+		container: string;
+		database: string;
+		domain: string;
+	};
 	collaborators: {
 		userId: string;
 		accreditation: string;
@@ -15,6 +20,11 @@ const appSchema = new mongoose.Schema<IApp>({
 	name: { type: String, required: true },
 	projectId: { type: mongoose.Schema.Types.ObjectId, ref: 'Project' },
 	image: { type: String },
+	resourcesPolicy: {
+		container: { type: mongoose.Schema.Types.ObjectId, ref: 'ResourcesPolicy' },
+		database: { type: mongoose.Schema.Types.ObjectId, ref: 'ResourcesPolicy' },
+		domain: { type: mongoose.Schema.Types.ObjectId, ref: 'ResourcesPolicy' },
+	},
 	collaborators: [{ userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, accreditation: { type: mongoose.Schema.Types.ObjectId, ref: 'Accreditation' } }],
 });
 
