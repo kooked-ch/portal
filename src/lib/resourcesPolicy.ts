1d2dfe39be11cdf145c90bf8c679d5908bb71db8
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-02 23:21:42 +0100

    feat: add function to retrieve app resources policy

--- src/lib/resourcesPolicy.ts
+++ src/lib/resourcesPolicy.ts
@@ -19,3 +19,46 @@ export async function getProjectResourcesPolicy(projectName: string): Promise<Pr
 	};
 }
 
+export async function getAppResourcesPolicy(projectName: string, appName: string): Promise<AppResourcesPolicy> {
+	const project = await ProjectModel.findOne({ slug: projectName }).exec();
+	if (!project) {
+		return {
+			containers: { name: '', description: '', totalLimit: 0, remainingLimit: 0 },
+			domains: { name: '', description: '', totalLimit: 0, remainingLimit: 0 },
+			databases: { name: '', description: '', totalLimit: 0, remainingLimit: 0 },
+		};
+	}
+
+	const appData = await customObjectsApi.getNamespacedCustomObject({ group: 'kooked.ch', version: 'v1', namespace: projectName, plural: 'kookedapps', name: appName });
+	const app = await AppModel.findOne({ name: appName, projectId: project._id }).exec();
+	if (!appData) {
+		return {
+			containers: { name: '', description: '', totalLimit: 0, remainingLimit: 0 },
+			domains: { name: '', description: '', totalLimit: 0, remainingLimit: 0 },
+			databases: { name: '', description: '', totalLimit: 0, remainingLimit: 0 },
+		};
+	}
+
+	const resourcesPolicy = await Promise.all([ResourcesPolicyModel.findOne({ _id: app.resourcesPolicy.container }).exec(), ResourcesPolicyModel.findOne({ _id: app.resourcesPolicy.domain }).exec(), ResourcesPolicyModel.findOne({ _id: app.resourcesPolicy.database }).exec()]);
+
+	return {
+		containers: {
+			name: resourcesPolicy[0]?.name || '',
+			description: resourcesPolicy[0]?.description || '',
+			totalLimit: resourcesPolicy[0]?.limitation.containers || 0,
+			remainingLimit: resourcesPolicy[0]?.limitation.containers - (appData.spec.containers ? appData.spec.containers.length : 0),
+		},
+		domains: {
+			name: resourcesPolicy[1]?.name || '',
+			description: resourcesPolicy[1]?.description || '',
+			totalLimit: resourcesPolicy[1]?.limitation.domains || 0,
+			remainingLimit: resourcesPolicy[1]?.limitation.domains - (appData.spec.domains ? appData.spec.domains.length : 0),
+		},
+		databases: {
+			name: resourcesPolicy[2]?.name || '',
+			description: resourcesPolicy[2]?.description || '',
+			totalLimit: resourcesPolicy[2]?.limitation.databases || 0,
+			remainingLimit: resourcesPolicy[2]?.limitation.databases - (appData.spec.databases ? appData.spec.databases.length : 0),
+		},
+	};
+}
