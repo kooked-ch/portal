39ee3112968779ab802bb18aec40a1407a96de22
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-05 16:16:48 +0100

    fix: implement user authentication and project retrieval in Home component

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,19 @@
-export default function Home() {
+'use server';
+
+import { getProjects } from '@/lib/project';
+import { getUser } from '@/lib/auth';
+import { ProjectsType } from '@/types/project';
+import ProjectsView from '@/components/ProjectsView';
+import { redirect } from 'next/navigation';
+
+export default async function Home() {
+	const user = await getUser();
+	if (!user) redirect('/login');
+	const projects: ProjectsType[] | null = await getProjects(user.id);
+
 	return (
-		<div className="container mx-auto px-28">
-			<h1>Projects</h1>
+		<div className="max-w-[1400px] mx-auto py-8 px-4">
+			<ProjectsView projects={projects} />
 		</div>
 	);
 }
