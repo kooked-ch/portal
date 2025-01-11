cdaa0ab6305de0aa4124fa4a5c8c2801d893cfab
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-11 17:40:02 +0100

    feat: implement deleteVolume function for volume management

--- src/lib/volume.ts
+++ src/lib/volume.ts
@@ -68,3 +68,63 @@ export async function createVolume(projectName: string, appName: string, data: V
 		return { message: 'An unexpected error occurred', status: 500 };
 	}
 }
+
+export async function deleteVolume(projectName: string, appName: string, volumeName: string): Promise<ErrorType> {
+	try {
+		const app: any = await customObjectsApi.getNamespacedCustomObject({
+			group: 'kooked.ch',
+			version: 'v1',
+			namespace: projectName,
+			plural: 'kookedapps',
+			name: appName,
+		});
+
+		if (!app) {
+			return {
+				message: 'App not found',
+				status: 404,
+			};
+		}
+
+		const containers = app?.spec?.containers || [];
+		const container = containers.find((container: any) => container.volumes?.some((volume: any) => volume.name === volumeName));
+
+		if (!container) {
+			return { message: 'Volume not found', status: 404 };
+		}
+
+		const volumes = containers.flatMap((container: any) => container.volumes || []);
+		const volume = volumes.find((volume: any) => volume.name === volumeName);
+
+		if (!volume) {
+			return { message: 'Volume not found', status: 404 };
+		}
+
+		const patch = [
+			{
+				op: 'replace',
+				path: `/spec/containers/${containers.indexOf(container)}/volumes`,
+				value: volumes.filter((volume: any) => volume.name !== volumeName),
+			},
+		];
+
+		await customObjectsApi.patchNamespacedCustomObject({
+			group: 'kooked.ch',
+			version: 'v1',
+			namespace: projectName,
+			plural: 'kookedapps',
+			name: appName,
+			body: patch,
+		});
+
+		log(`Deleted volume ${volumeName}`, 'info', projectName, appName);
+
+		return {
+			message: 'Volume deleted',
+			status: 200,
+		};
+	} catch (error: unknown) {
+		console.error('Error deleting database:', error);
+		return { message: 'An unexpected error occurred', status: 500 };
+	}
+}
