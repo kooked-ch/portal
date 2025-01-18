40c5eb2bb49c688c65b55f3813a059932af53065
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-18 16:30:16 +0100

    feat: add generateRandomString utility function for random string generation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -34,3 +34,12 @@ export const getRepository = async (url: string) => {
 		return null;
 	}
 };
+
+export const generateRandomString = (length: number) => {
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let result = '';
+	for (let i = 0; i < length; i++) {
+		result += characters.charAt(Math.floor(Math.random() * characters.length));
+	}
+	return result;
+};
