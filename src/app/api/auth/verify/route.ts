d2d4088067dfdee8a9d0ba65a8313f54c59f6489
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-18 16:33:42 +0100

    feat: implement user email verification endpoint

---
+++ src/app/api/auth/verify/route.ts
@@ -0,0 +1,28 @@
+import db from '@/lib/mongo';
+import { UserModel } from '@/models/User';
+import { NextResponse } from 'next/server';
+import { EmailModel } from '@/models/Email';
+
+export async function POST(req: Request, res: Response) {
+	try {
+		const { code, email } = await req.json();
+		await db.connect();
+
+		const user = await UserModel.findOne({ email }).exec();
+		if (!user) return NextResponse.json({ error: 'User not found' }, { status: 404 });
+
+		const mail = await EmailModel.findOne({ email, token: code }).exec();
+		if (!mail) return NextResponse.json({ error: 'Invalid code' }, { status: 400 });
+
+		await UserModel.updateOne({ email }, { verified: true }).exec();
+
+		const userUpdated = await UserModel.findOne({
+			email,
+		}).exec();
+
+		return NextResponse.json({ message: 'User verified' });
+	} catch (error) {
+		console.error('Error during validation:', error);
+		return NextResponse.json({ error: 'Error during validation' });
+	}
+}
