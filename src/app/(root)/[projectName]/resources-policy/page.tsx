6c697a6cdbbcea0be6bc052da483c172e10e86d8
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-02-16 18:23:00 +0100

    feat: create ResourcesPolicyPage component for managing project resource policies

---
+++ src/app/(root)/[projectName]/resources-policy/page.tsx
@@ -0,0 +1,23 @@
+'use server';
+import ResourcesPolicyEditor from '@/components/resourcesPolicy';
+import { checkAccreditation } from '@/lib/auth';
+import { getAllProjectResourcesPolicy } from '@/lib/resourcesPolicy';
+import { redirect } from 'next/navigation';
+
+export default async function ResourcesPolicyPage({ params }: { params: { projectName: string } }) {
+	if (!(await checkAccreditation('resourcesPolicy:0:read'))) {
+		return <div className="text-muted-foreground text-center justify-center flex items-center h-[calc(100vh-100px)]">Unable to access this page</div>;
+	}
+
+	const allProjectResourcesPolicy = await getAllProjectResourcesPolicy(params.projectName);
+
+	if (!allProjectResourcesPolicy) {
+		return <div className="text-muted-foreground text-center justify-center flex items-center h-[calc(100vh-100px)]">Failed to load resource policy</div>;
+	}
+
+	return (
+		<div className="xl:container mx-auto px-3 xl:px-28 md:py-8 py-3">
+			<ResourcesPolicyEditor allProjectResourcesPolicy={allProjectResourcesPolicy} projectName={params.projectName} />
+		</div>
+	);
+}
