71c2fc39a7c1e7d611ca92fd7bea348412d3d77a
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-02-17 21:35:14 +0100

    feat: enhance CollaboratorsTab component with disabled state and improved accreditation checks

--- src/components/collaborator.tsx
+++ src/components/collaborator.tsx
@@ -5,14 +5,14 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@
 import { AppType } from '@/types/app';
 import { AccreditationType } from '@/types/accreditations';
 
-export default function CollaboratorsTab({ collaborators, accreditations }: { collaborators: AppType['collaborators']; accreditations: AccreditationType[] }) {
+export default function CollaboratorsTab({ collaborators, accreditations, disabled }: { collaborators: AppType['collaborators']; accreditations: AccreditationType[]; disabled: boolean }) {
 	return (
 		<ul className="space-y-2">
 			{collaborators.map((collaborator) => {
 				return (
 					<li key={collaborator.username} className="bg-[#18181a] rounded-lg flex justify-between px-4 py-3 items-center h-[68px] border-l-4 border-blue-500">
-						<div className="flex items-center space-x-3">
-							<Avatar className="h-8 w-8">
+						<div className="flex items-center space-x-3 w-1/4">
+							<Avatar className="h-10 w-10">
 								<AvatarImage src={collaborator.image} />
 								<AvatarFallback>{collaborator.username.slice(0, 2).toUpperCase()}</AvatarFallback>
 							</Avatar>
@@ -20,15 +20,15 @@ export default function CollaboratorsTab({ collaborators, accreditations }: { co
 								<span className="text-white font-medium">{collaborator.username}</span>
 							</div>
 						</div>
-						<div className="flex items-center space-x-3 text-gray-500">
-							{collaborator.accreditation.authorizations.containers.length === 4 && <Container className="w-5 h-5" />}
-							{collaborator.accreditation.authorizations.databases.length === 4 && <Database className="w-5 h-5" />}
-							{collaborator.accreditation.authorizations.domains.length === 4 && <Globe className="w-5 h-5" />}
-							{collaborator.accreditation.authorizations.secrets.length === 2 && <KeyRound className="w-5 h-5" />}
-							{collaborator.accreditation.authorizations.logs.length === 1 && <Text className="w-5 h-5" />}
-							{collaborator.accreditation.authorizations.collaborators.length === 4 && <UsersRound className="w-5 h-5" />}
+						<div className="flex items-center justify-start space-x-3 text-gray-500">
+							{collaborator.accreditation?.authorizations?.containers?.length === 4 && <Container className="w-5 h-5" />}
+							{collaborator.accreditation?.authorizations?.databases?.length === 4 && <Database className="w-5 h-5" />}
+							{collaborator.accreditation?.authorizations?.domains?.length === 4 && <Globe className="w-5 h-5" />}
+							{collaborator.accreditation?.authorizations?.secrets?.length === 2 && <KeyRound className="w-5 h-5" />}
+							{collaborator.accreditation?.authorizations?.logs?.length === 1 && <Text className="w-5 h-5" />}
+							{collaborator.accreditation?.authorizations?.collaborators?.length === 4 && <UsersRound className="w-5 h-5" />}
 						</div>
-						<Select defaultValue={collaborator.accreditation.slug} onValueChange={(value) => console.log(value)}>
+						<Select defaultValue={collaborator.accreditation.slug} disabled={true} onValueChange={(value) => console.log(value)}>
 							<SelectTrigger className="w-40">
 								<SelectValue />
 							</SelectTrigger>
